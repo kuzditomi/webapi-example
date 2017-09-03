e4a44722001bca07727eea1d8a9c1161081f1af2
Küzdi Tamás <kuzditomi@gmail.com>
2017-09-03 22:35:04 +0200

    add generator and start client

---
+++ Example/ClientGenerator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using NSwag;
+using NSwag.CodeGeneration.CSharp;
+
+namespace ClientGenerator
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var document = await SwaggerDocument.FromUrlAsync("http://localhost:49530/swagger/docs/v1");
+
+            var settings = new SwaggerToCSharpClientGeneratorSettings
+            {
+                ClassName = "CarClient",
+                CSharpGeneratorSettings = { Namespace = "Example" }
+            };
+
+            var generator = new SwaggerToCSharpClientGenerator(document, settings);
+
+            var code = generator.GenerateFile();
+            var clientFilePath = Path.Combine(Assembly.GetExecutingAssembly().Location, "../../../CarClient.cs");
+                
+            if (!File.Exists(clientFilePath))
+            {
+                File.Create(clientFilePath);
+            }
+
+            using (var tw = new StreamWriter(clientFilePath, false))
+            {
+                await tw.WriteAsync(code).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ Example/TestClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Example;
+
+namespace TestClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new Example.CarClient();
+
+            var cars = await client.GetCarsAsync().ConfigureAwait(false);
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car.Id + "-" + car.PlateNumber);
+            }
+
+            try
+            {
+                await client.AddCarAsync(new CreateCar
+                {
+                    PlateNumber = "ABB-123",
+                    Color = CreateCarColor.Red
+                }).ConfigureAwait(false);
+            }
+            catch (SwaggerException e)
+            {
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
