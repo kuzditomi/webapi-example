e8d911f96da3d155c293a30a8192384828693d65
Küzdi Tamás <kuzditomi@gmail.com>
2017-09-03 16:29:10 +0200

    add simple car get operation

--- Example/WebAPI/Controllers/CarsController.cs
+++ Example/WebAPI/Controllers/CarsController.cs
@@ -1,10 +1,10 @@
-﻿using System.CodeDom;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Repository;
+using WebAPI.ViewModels;
 
 namespace WebAPI.Controllers
 {
@@ -31,14 +31,42 @@ namespace WebAPI.Controllers
         {
             var cars = await this._repository.GetAllCars().ConfigureAwait(false);
 
-            var result = cars.Select(car => new ViewModels.Car
+            var result = cars.Select(car => new Car
             {
                 Id = car.Id,
                 PlateNumber = car.PlateNumber,
-                Color = (ViewModels.CarColor)car.Color
+                Color = (CarColor)car.Color
             });
 
             return Ok(result);
+        }
+
+        /// <summary>
+        /// Gets car by it's id in the system
+        /// </summary>
+        /// <param name="id">Id of car in system</param>
+        /// <response code="200">Returns the car</response>
+        /// <response code="404">If car with given Id is not found</response>
+        [HttpGet]
+        [Route("{id}")]
+        [ResponseType(typeof(Car)), ResponseType(typeof(string))]
+        public async Task<IHttpActionResult> GetCarById(int id)
+        {
+            var cars = await this._repository.GetAllCars().ConfigureAwait(false);
+
+            var car = cars.SingleOrDefault(c => c.Id == id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(new Car
+            {
+                Id = car.Id,
+                PlateNumber = car.PlateNumber,
+                Color = (CarColor) car.Color
+            });
         }
     }
 }
